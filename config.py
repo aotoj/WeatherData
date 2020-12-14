7d1a9be18a215118807c004e28e4abc5300b1eba
aotoj <40181994+aotoj@users.noreply.github.com>
2020-12-13 20:39:21 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-weather_api_key="d4cb209770713b56aa45c40025b8cc28"
+weather_api_key="46e9df3ab43746a4a5ac2df0c0968378"
 #from config import api_key, g_key
-g_key = "AIzaSyBPt2FpiXw740J-Vh6vmtgVGLj0k9cGpOA"
